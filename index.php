b40e8c6a1ebb90772ed687a493a18b58e1de8be4
Juanjo <jujomilo@gmail.com>
2024-12-11 16:42:17 +0100

    dynamic routes

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
 <?php
+
+// Analizar si existe alguna petición vía GET del usuario
+if( isset($_GET['controlador']) && isset($_GET['accion']) )
+{
+   // Si me llega el string "controlador", lo guardo en una variable del mismo nombre
+   $controlador = $_GET['controlador'];
+   // Si me llega el string "accion", lo guardo en una variable del mismo nombre
+   $accion = $_GET['accion'];
+
+   // depuración imprimo en pantalla qué me está llegando
+   //print_r($_GET);
+}
+
+
  require_once("vistas/template.php");
  ?>
--- ruteador.php
+++ ruteador.php
@@ -1,12 +1,22 @@
 <?php 
 
-// 1 incluimos nuestro controlador
-include_once("controladores/controlador_paginas.php");
+// Verificar que nos lleganlas solicitudes vía GET  Solicitud  NO CONFUNDIR CON INSTANCIA $controlador
+//echo $controlador; // controlador recibido por URL
+//echo $accion;  // acción recibida por URL
 
-// 2 instanciamos un Objeto de la clase ControladorPaginas que tenemos en el archivo controlador_paginas
-$controlador = new ControladorPaginas();
+//:::::::::::::DEFINIR ELL CONTROLADOR DE FORMA DINÁMICA::::::::::::::::::::::::::::::::
+// cargamos el contorlador que llegue via GET
+include_once("controladores/controlador_".$controlador.".php");  
 
+// Componer el nombre d el ainstancia
+$objControlador = "Controlador".ucfirst($controlador);  // = (NOMBRE DE LA CLASE) Controlador + Lo que venga en URL con la primera en mayúscula
+
+// 2 instanciamos un Objeto de la clase que venga por URL que tenemos en el archivo controlador_paginas
+$controlador = new $objControlador();  
+
+
+//:::::::::::::DEFINIR LA ACCIÓN DE FORMA DINÁMICA::::::::::::::::::::::::::::::::::::::
 //con esta instancia de la clase llamo al método inicio
-$controlador->inicio();
+$controlador->$accion();  // Se ejecuta la acción que se pieda por URL
 
 ?>
